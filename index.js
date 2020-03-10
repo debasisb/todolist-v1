e1521c49b7682ea94b64622803eecd66c93deebe
yasharonquillo <yasharon@hawaii.edu>
2020-03-10 23:18:58 +0000

    Exam 2 items

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const app = express();
 // set an array for the default items in the list
 let items = ["Buy Food", "Prepare Food", "Cook Food", "Eat Food"];
 // set an empty array for new work items
-let workItems = ["Show Up", "Get Settled"];
+//let workItems = ["Show Up", "Get Settled"];
+
+let weekendItems = ["Do laundry", "Hangout with Family"];
+let weekdayItems = ["Go to work", "Go to school"];
 
 // set EJS as the viewing engine to display html
 app.set('view engine', 'ejs');
@@ -23,15 +26,15 @@ app.use(express.static("public"));
 
 
 // default html file in web server
-app.get("/", function(req, res) {
+// app.get("/", function(req, res) {
 
-    //get the system date from the getDate function exported by the date.js file
-    let day = date.getDate();
+//     //get the system date from the getDate function exported by the date.js file
+//     let day = date.getDate();
     
-    // use EJS render to display the day and the To Do List
-    res.render("list", {listTitle: day, newListItems: items});
+//     // use EJS render to display the day and the To Do List
+//     res.render("list", {listTitle: day, newListItems: items});
     
-});
+// });
 
 // display default to do list on the default root folder
 app.post("/", function(req, res) {
@@ -40,9 +43,13 @@ app.post("/", function(req, res) {
     // code allows items to be added to the regular list and work list
     let item = req.body.newItem;
     
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
+    if (req.body.list === "Weekend") {
+        weekendItems.push(item);
+        res.redirect("/weekend");
+    }
+    else if (req.body.list === "Weekday") {
+        weekdayItems.push(item);
+        res.redirect("/weekday");
     } else {
         items.push(item);
         res.redirect("/");
@@ -50,8 +57,16 @@ app.post("/", function(req, res) {
 });
 
 // display default to do list on the localhost:3000/work route!
-app.get("/work", function(req, res){
-    res.render("list", {listTitle: "Work To Do List", newListItems: workItems})
+// app.get("/work", function(req, res){
+//     res.render("list", {listTitle: "Work To Do List", newListItems: workItems})
+// });
+
+app.get("/weekend", function(req, res){
+    res.render("list", {listTitle: "Weekend List", newListItems: weekendItems})
+});
+
+app.get("/weekday", function(req, res){
+    res.render("list", {listTitle: "Weekday List", newListItems: weekdayItems})
 });
 
 app.listen(3000, function() {
